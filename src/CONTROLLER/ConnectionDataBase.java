a9c64f5d514ea704dd3bd10a63af46efcc6cad61
Iago Massucato <iagomassucatojl@gmail.com>
2023-07-18 19:38:40 -0300

    1º VERSÃO

---
+++ src/CONTROLLER/ConnectionDataBase.java
@@ -0,0 +1,31 @@
+package CONTROLLER;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Microsoft
+ */
+public class ConnectionDataBase {
+    public Connection connectionBD(){
+        Connection conn = null;
+        try {
+            String url = "jdbc:mysql://localhost:3306/login?user=root&password=";
+            conn = DriverManager.getConnection(url);
+        } catch (SQLException error) {
+            JOptionPane.showMessageDialog(null, "ConnectionDataBase"+error.getMessage());
+        }
+        
+        return conn;
+    }
+    
+}
